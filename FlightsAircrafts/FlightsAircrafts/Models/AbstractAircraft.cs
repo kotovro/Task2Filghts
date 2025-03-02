08f898be96f51f5b847e81d27212c70bfd87bdb6
Kotvoro <kotvoro@gmail.com>
2025-03-02 12:28:23 +0300

    Adds simplest models for aircrafts

---
+++ FlightsAircrafts/FlightsAircrafts/Models/AbstractAircraft.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightsAircrafts.Models
+{
+    abstract class AbstratcAircraft
+    {
+        public abstract double CurrentHeight { get; set; }
+        public abstract bool Takeoff();
+        public abstract bool Land();
+
+        public override string ToString()
+        {
+            return $"{CurrentHeight}";
+        }
+    }
+}
---
+++ FlightsAircrafts/FlightsAircrafts/Models/Airplane.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightsAircrafts.Models
+{
+    class Airplane : AbstratcAircraft
+    {
+
+        public override double CurrentHeight { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override bool Land()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Takeoff()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
