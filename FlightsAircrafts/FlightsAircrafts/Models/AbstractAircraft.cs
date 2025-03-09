166c6c649f1ea3e52b7dfe93a0f5c534901d4678
Kotvoro <kotvoro@gmail.com>
2025-03-09 10:13:02 +0300

    Adds deterioration levels and correwpsonding properties to AbstractAircraft and Plane

--- FlightsAircrafts/FlightsAircrafts/Models/AbstractAircraft.cs
+++ FlightsAircrafts/FlightsAircrafts/Models/AbstractAircraft.cs
@@ -8,13 +8,21 @@ namespace FlightsAircrafts.Models
 {
     abstract class AbstratcAircraft
     {
+        public static class DetoriationLevel
+        {
+            public const double HIGH = 100.0;
+            public const double MEDIUM = 65.0;
+            public const double LOW = 30.0;
+        }
+
         public abstract double CurrentHeight { get; set; }
+        protected abstract double CurrentDetoriationLevel { get; set; }
         public abstract bool Takeoff();
         public abstract bool Land();
 
         public override string ToString()
         {
-            return $"{CurrentHeight}";
+            return $"Current height is:  {CurrentHeight} \n Cueerent detoriation level: {CurrentDetoriationLevel}" ;
         }
     }
 }
--- FlightsAircrafts/FlightsAircrafts/Models/Airplane.cs
+++ FlightsAircrafts/FlightsAircrafts/Models/Airplane.cs
@@ -8,7 +8,16 @@ namespace FlightsAircrafts.Models
 {
     class Airplane : AbstratcAircraft
     {
+        public Airplane(double fuelLevel, double detoriationLevel, double runwayLength)
+        {
+            RunwayLength = runwayLength;
+            FuelLevel = fuelLevel;
+            CurrentDetoriationLevel = detoriationLevel; 
+        }
 
+        protected override double CurrentDetoriationLevel { get; set; }
+        protected double FuelLevel { get; set; }
+        private double RunwayLength  { get; set; }
         public override double CurrentHeight { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
         public override bool Land()
@@ -18,7 +27,52 @@ namespace FlightsAircrafts.Models
 
         public override bool Takeoff()
         {
-            throw new NotImplementedException();
+            Random random = new Random();
+            double probability = random.NextDouble();
+            ///add DetoriationLevelEnum
+            if (FuelLevel > 0)
+            {
+                if (CurrentDetoriationLevel <= DetoriationLevel.LOW)
+                {
+                    return true;
+                }
+                if (CurrentDetoriationLevel > DetoriationLevel.LOW && CurrentDetoriationLevel <= DetoriationLevel.MEDIUM)
+                {
+                    if (probability > 0.4)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+
+                if (CurrentDetoriationLevel > DetoriationLevel.MEDIUM && CurrentDetoriationLevel <= DetoriationLevel.HIGH)
+                {
+                    if (probability > 0.8)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+
+                if (CurrentDetoriationLevel > DetoriationLevel.HIGH)
+                {
+                    if (probability > 0.95)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            return false;
         }
     }
 }
